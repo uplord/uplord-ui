3740b2221f37a78f97b12403f04f9ee1b916937c
Michael <michael@uplord.co.uk>
2025-08-16 09:24:26 +0100

    feat: remove scroll export

--- src/components/index.ts
+++ src/components/index.ts
@@ -21,5 +21,3 @@ export { Textarea } from './ui/Textarea'
 export { Example } from './utils/Example'
 export { Icon } from './utils/Icon'
 export { Modal } from './utils/Modal'
-
-export { useScroll } from '@/lib/scrollUtils'
