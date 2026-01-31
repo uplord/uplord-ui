e2b00cd710cf167428464fcaf4d516d2aec3d2ce
Michael <michael@uplord.co.uk>
2026-01-31 12:07:12 +0000

    feat: styles amends

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import styles from './page.module.css'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Home</h1>
+      <Button label="Test" />
     </div>
   )
 }
